db2ed922e6c3c2fbfc3a61f49ee3c2755596e876
Deltace05 <ben.mackenzie05@gmail.com>
2022-05-26 09:57:52 +1000

    Update SmartDogSketch.ino

--- SmartDogSketch/SmartDogSketch.ino
+++ SmartDogSketch/SmartDogSketch.ino
@@ -9,7 +9,7 @@ DateTime rightNow;
 // Traffic Lights - LED Outputs
 #define ledRed A0
 #define ledYellow A1
-#define ledGreen A2 
+#define ledGreen A2
 // Pin definition
 const unsigned int IN1 = 5;
 const unsigned int IN2 = 6;
@@ -31,38 +31,38 @@ Servo myservo;
 #define crashSensor 4
 
 void setup() {
-Serial.begin(9600);
-while (!Serial) {
-  delay(1);
-}
- Serial.print("Initializing SD card...");
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(1);
+  }
+  Serial.print("Initializing SD card...");
   if (!SD.begin(10)) {
     Serial.println("initialization failed!");
     while (1);
   }
   rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
   Serial.println("initialization done.");
-logEvent("System Initialisation...");
+  logEvent("System Initialisation...");
 
-// Traffic Lights - LED Outputs
-pinMode(ledRed, OUTPUT);
-pinMode(ledYellow, OUTPUT);
-pinMode(ledGreen, OUTPUT);
-// DC Motor & Motor Module - L298N
-motor.setSpeed(70);
-// Servo
-myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-//Potentiometer
-pinMode(pot, INPUT);
-// Piezo Buzzer
-pinMode(piezoPin,OUTPUT);
-// Sonar - HC-SR04
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-// Line Sensor
-pinMode(lineSensorPin, OUTPUT);
-// Crash Sensor / Button
-pinMode(crashSensor, INPUT);
+  // Traffic Lights - LED Outputs
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  // DC Motor & Motor Module - L298N
+  motor.setSpeed(70);
+  // Servo
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  //Potentiometer
+  pinMode(pot, INPUT);
+  // Piezo Buzzer
+  pinMode(piezoPin, OUTPUT);
+  // Sonar - HC-SR04
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  // Line Sensor
+  pinMode(lineSensorPin, OUTPUT);
+  // Crash Sensor / Button
+  pinMode(crashSensor, INPUT);
 
 }
 
